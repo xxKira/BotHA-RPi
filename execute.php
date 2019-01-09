5e52fc706fe7b65b6583a46f2561d7f44682ac96
RWGaMeR <puxxo9700@gmail.com>
2019-01-09 17:53:44 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -190,3 +190,4 @@ header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
+?>
